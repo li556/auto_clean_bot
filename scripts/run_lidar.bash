0a308c0d68bc3e6cb69b7c1cce6bd3d0961baab7
Yangsh <yangsh@ai-dynamics.cn>
2024-10-14 17:19:03 +0800

    change the initial frame and topic name of lidar

--- scripts/run_lidar.bash
+++ scripts/run_lidar.bash
@@ -2,4 +2,4 @@
 
 source install/setup.bash
 # Run the lidar node
-ros2 launch innovusion ivu_pc2.py device_ip:=172.168.1.10
+ros2 launch innovusion ivu_pc2.py device_ip:=172.168.1.10 frame_id:=base_lidar output_topic:=front_lidar
