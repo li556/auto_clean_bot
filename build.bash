8d7d7b1ee2bbce25e751ca21c7d5af2698721c30
Yangsh <yangsh@ai-dynamics.cn>
2024-10-21 10:14:24 +0800

    add localization node code

---
+++ build.bash
@@ -0,0 +1,3 @@
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev \
+                     libhdf5-serial-dev protobuf-compiler \
+                     libatlas-base-dev libboost-all-dev libgflags-dev libgoogle-glog-dev liblmdb-dev
